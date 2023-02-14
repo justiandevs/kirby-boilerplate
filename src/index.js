d21aab62210287767471f45e113490e790a999b5
justiandevs <justiandev@gmail.com>
2023-02-14 12:10:16 +0100

    BarbaJS and GSAP integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import barba from '@barba/core';
+import gsap from 'gsap';
 import './index.css'
+
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0,
+        display: "none"
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0,
+      });
+    }
+  }]
+});
 
 console.log('Hey Kirby, Twig, BarbaJS, GSAP and SCSS!')
